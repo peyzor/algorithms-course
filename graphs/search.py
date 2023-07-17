ab382dd87ec177e74879154f607cfbf978b62bce
Peyman <hassani.peyman627@gmail.com>
2023-07-17 10:35:28 +0330

    Add dfs search on graph

--- graphs/search.py
+++ graphs/search.py
@@ -35,6 +35,37 @@ def bfs(graph, source, needle):
     return [source, *out]
 
 
+class Edge:
+    def __init__(self, destination):
+        self.to = destination
+
+
+def walk(graph, curr, needle, seen, path):
+    if seen[curr]:
+        return False
+
+    seen[curr] = True
+    path.append(curr)
+    if curr == needle:
+        return True
+
+    list = graph[curr]
+    for i in range(len(list)):
+        if walk(graph, list[i], needle, seen, path):
+            return True
+
+    path.pop()
+    return False
+
+
+def dfs(graph, source, needle):
+    seen = [False for _ in range(len(graph))]
+    path = []
+
+    walk(graph, source, needle, seen, path)
+    return path
+
+
 def main():
     graph = [
         [0, 1, 1, 0, 0, 0],
@@ -50,6 +81,19 @@ def main():
 
     out = bfs(graph, source, needle)
     print(out)
+
+    graph2 = {
+        0: [1, 2],
+        1: [],
+        2: [3],
+        3: []
+    }
+
+    g2_source = 0
+    g2_needle = 3
+
+    g2_out = dfs(graph2, g2_source, g2_needle)
+    print(g2_out)
 
 
 if __name__ == "__main__":
